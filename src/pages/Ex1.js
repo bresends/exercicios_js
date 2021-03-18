efdeb06a8508c8cc151871fe698a5dc562d5bc50
Bruno <bruno.resendesantos45@gmail.com>
2021-03-17 21:12:23 -0300

    Form Básico Funcionando

--- src/pages/Ex1.js
+++ src/pages/Ex1.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../components/Card/Card';
 
 const Ex1 = () => {
+  const [valorInput, setValorInput] = useState('');
+  const [respostaFinal, setRespostaFinal] = useState('');
+
+  function entregarResposta(e) {
+    e.preventDefault();
+    valorInput
+      ? setRespostaFinal(`Olá ${valorInput}!`)
+      : setRespostaFinal('Por favor, digite algo');
+
+    setValorInput('');
+  }
   return (
     <Card>
-      <h1>Exercicio 1</h1>
-      <p>Entrada</p>
-      <form>
-        <input type="text" name="input" />
-        <input type="submit" value="Clique" className="button" />
+      <h2 className="center-card__title">Exercicio 1 - Respondendo o usuário</h2>
+      <p className="center-card__question-title">Entre com seu nome</p>
+
+      <form className="center-card__form">
+        <input
+          type="text"
+          name="input"
+          value={valorInput}
+          onChange={(e) => setValorInput(e.target.value)}
+          className="center-card__text-input"
+        />
+        <input
+          type="submit"
+          value="Clique"
+          onClick={entregarResposta}
+          className="center-card__button"
+        />
       </form>
-      <div className="container-resposta">
-        <p>Resposta: </p>
-        <p>Lorem ipsum dolor sit amet </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+
+      <div className="center-card__answer">
+        <p className="answer__default-answer">Console:</p>
+        <p className="answer__final">{respostaFinal}</p>
       </div>
     </Card>
   );
