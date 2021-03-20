7007d45c49ca6f8aed94919fb6cc28a3019307ea
Bruno <bruno.resendesantos45@gmail.com>
2021-03-20 10:20:45 -0300

    Estilo Refatorado e Ex 2 Pronto

--- src/pages/Ex1.js
+++ src/pages/Ex1.js
@@ -1,5 +1,19 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import Card from '../components/Card/Card';
+
+const DivStyle = styled.div`
+  .center-card__form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  .center-card__input {
+    width: 100%;
+    margin: 10px auto;
+  }
+`;
 
 const Ex1 = () => {
   const [valorInput, setValorInput] = useState('');
@@ -15,24 +29,28 @@ const Ex1 = () => {
   }
   return (
     <Card>
-      <h2 className="center-card__title">Exercicio 1 - Respondendo o usuário</h2>
+      <h2 className="center-card__title">
+        Exercicio 1 - Respondendo o usuário
+      </h2>
       <p className="center-card__question-title">Entre com seu nome</p>
 
-      <form className="center-card__form">
-        <input
-          type="text"
-          name="input"
-          value={valorInput}
-          onChange={(e) => setValorInput(e.target.value)}
-          className="center-card__text-input"
-        />
-        <input
-          type="submit"
-          value="Clique"
-          onClick={entregarResposta}
-          className="center-card__button"
-        />
-      </form>
+      <DivStyle>
+        <form className="center-card__form">
+          <input
+            type="text"
+            name="input"
+            value={valorInput}
+            onChange={(e) => setValorInput(e.target.value)}
+            className="center-card__text-input"
+          />
+          <input
+            type="submit"
+            value="Clique"
+            onClick={entregarResposta}
+            className="center-card__button"
+          />
+        </form>
+      </DivStyle>
 
       <div className="center-card__answer">
         <p className="answer__default-answer">Console:</p>
--- src/pages/Ex2.js
+++ src/pages/Ex2.js
@@ -1,21 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../components/Card/Card';
+import styled from 'styled-components';
+
+const DivStyle = styled.div`
+  .center-card__form {
+    display: flex;
+    gap: 1rem;
+  }
+
+  .center-card__input-flex-container {
+    width: 100%;
+  }
+
+  .center-card__input {
+    width: 100%;
+  }
+
+  .center-card__button {
+    margin-top: 1rem;
+    width: 100%;
+  }
+`;
 
 const Ex2 = () => {
+  const [valorInput1, setValorInput1] = useState('');
+  const [valorInput2, setValorInput2] = useState('');
+  const [respostaFinal, setRespostaFinal] = useState('');
+
+  function retornaSoma() {
+    const valorSoma =
+      Number.parseInt(valorInput1) + Number.parseInt(valorInput2);
+    const stringResposta = `A soma de ${valorInput1} e ${valorInput2} é ${valorSoma}`;
+    setRespostaFinal(stringResposta);
+  }
+
   return (
-      <Card>
-        <h1>Exercicio 2</h1>
-        <p>Entrada</p>
-        <form>
-          <input type="text" name="input" />
-          <input type="submit" value="Clique" className="button" />
+    <Card>
+      <h2 className="center-card__title">Exercicio 2 - Somando números</h2>
+
+      <DivStyle>
+        <form className="center-card__form">
+          <div className="center-card__input-flex-container">
+            <label htmlFor="numero1" className="center-card__input-title">
+              Entre com o número 1
+            </label>
+
+            <input
+              type="number"
+              name="numero1"
+              value={valorInput1}
+              onChange={(e) => setValorInput1(e.target.value)}
+              className="center-card__input"
+            />
+          </div>
+
+          <div className="center-card__input-flex-container">
+            <label htmlFor="numero2" className="center-card__input-title">
+              Entre com o número 2
+            </label>
+
+            <input
+              type="number"
+              name="numero2"
+              value={valorInput2}
+              onChange={(e) => setValorInput2(e.target.value)}
+              className="center-card__input"
+            />
+          </div>
         </form>
-        <div className="container-resposta">
-          <p>Resposta: </p>
-          <p>Lorem ipsum dolor sit amet </p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-        </div>
-      </Card>
+        <input
+          type="submit"
+          value="Clique"
+          className="center-card__button"
+          onClick={retornaSoma}
+        />
+      </DivStyle>
+
+      <div className="center-card__answer">
+        <p className="answer__default-answer">Console:</p>
+        <p className="answer__final">{respostaFinal}</p>
+      </div>
+    </Card>
   );
 };
 
